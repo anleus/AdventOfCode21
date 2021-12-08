4a2568ad96760ccd4c64bc99e28edfd8ed15adec
Tomás Ruiz <tomas.rui598@gmail.com>
2021-12-08 12:52:17 +0100

    uy

---
+++ AdventOfCode/Puzzles/Day8/Puzzle8.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using Linq;
+
+class Puzzle8 
+{
+    public static int Part1(string path) 
+    {
+        string[] rows = File.ReadAllLines(path).ToArray();
+
+        rows
+
+
+    }
+}
